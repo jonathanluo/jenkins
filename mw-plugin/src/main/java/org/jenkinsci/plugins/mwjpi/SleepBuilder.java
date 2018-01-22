c2abc57178fc7d19ec33ebe9285bfb57cd10ced2
moonwave <moonwave@gmail.com>
2018-01-22 15:36:41 -0800

    select (drop-down menu)

--- mw-plugin/src/main/java/org/jenkinsci/plugins/mwjpi/SleepBuilder.java
+++ mw-plugin/src/main/java/org/jenkinsci/plugins/mwjpi/SleepBuilder.java
@@ -1,9 +1,12 @@
 package org.jenkinsci.plugins.mwjpi;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
+import org.jenkinsci.plugins.mwjpi.model.BuildGoal;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
@@ -17,22 +20,27 @@ import hudson.model.FreeStyleProject;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
 
 /**
  * https://stackoverflow.com/questions/12236909/the-package-collides-with-a-type
- *
+ * SleepBuilder is a class name in main/java, and a package name in main/resources
+ * To avoid seeing the warning message "the package collides with a type", comment out the following line in .classpath:
+ * <!--  <classpathentry kind="src" path="src/main/resources" excluding="** /*.java"/> -->
  */
 public class SleepBuilder extends Builder {
     private long time;
     private String testName;
+    private String testType;
 
     /**
      * Match by field name, argument order is not important 
      */
     @DataBoundConstructor
-    public SleepBuilder(String testName, long time) { // goes here when Save / Apply clicked during Job configuration
+    public SleepBuilder(String testName, long time, String testType) { // goes here when Save / Apply clicked during Job configuration
         this.time = time;
         this.testName = testName;
+        this.testType = testType;
     }
 
     public long getTime() { // goes here when Config clicked if previous saved Job contains SleepBuilder instance
@@ -49,6 +57,14 @@ public class SleepBuilder extends Builder {
 
     public void setTestName(String testName) {
         this.testName = testName;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+    public void setTestType(String testType) {
+        this.testType = testType;
     }
 
     @Override
@@ -81,12 +97,14 @@ public class SleepBuilder extends Builder {
         }
 
         /**
-         * https://wiki.eclipse.org/Jelly_form_controls - how to populate other fields in jenkins plugin
+         * https://wiki.eclipse.org/Jelly_form_controls
+         *    - how to populate other fields in jenkins plugin
+         *    - client side Javascript
+         *
+         * https://wiki.jenkins.io/display/JENKINS/Form+Validation
+         * the method gets invoked in response to the onchange event on HTML DOM
          */
-        public FormValidation doCheckTime(@QueryParameter("time") String time, // goes here when config intial page load, on change, Apply
-                                          @QueryParameter("testName") String test_name,
-                                          @QueryParameter String testType
-                )
+        public FormValidation doCheckTime(@QueryParameter("time") final String time) // goes here when config intial page load, on change, Apply
                 throws IOException, ServletException {
             try {
                 if (Long.valueOf(time) < 0) {
@@ -102,7 +120,7 @@ public class SleepBuilder extends Builder {
             return FormValidation.error(Messages.SleepBuilder_DescriptorImpl_errors_enterNumber());
         }
 
-        public FormValidation doCheckTestName(@QueryParameter String testName, @AncestorInPath AbstractProject project)
+        public FormValidation doCheckTestName(@QueryParameter final String testName, @AncestorInPath AbstractProject project)
                 throws IOException, ServletException {
             try {
                 if (testName.startsWith("Test")) {
@@ -112,7 +130,7 @@ public class SleepBuilder extends Builder {
                 }
             } catch (NumberFormatException e) {
             }
-            return FormValidation.error(Messages.SleepBuilder_DescriptorImpl_errors_enterNumber());
+            return FormValidation.error("Unknown error occurred");
         }
 
         /**
@@ -123,7 +141,7 @@ public class SleepBuilder extends Builder {
          * FreeStyleProject object while validating a field of a Builder. You do this by putting 
          * AncestorInPath annotation.
          */
-        public FormValidation doCheckTestType(@QueryParameter("testType") String testType, @AncestorInPath AbstractProject project)
+        public FormValidation doCheckTestType(@QueryParameter("testType") final String testType, @AncestorInPath AbstractProject project)
                 throws IOException, ServletException {
             try {
                 if (testType.equals("unit") || testType.equals("regression") || testType.equals("functional")) {
@@ -133,7 +151,61 @@ public class SleepBuilder extends Builder {
                 }
             } catch (NumberFormatException e) {
             }
-            return FormValidation.error(Messages.SleepBuilder_DescriptorImpl_errors_enterNumber());
+            return FormValidation.error("Unknown error occurred");
+        }
+
+        /**
+         * https://wiki.jenkins.io/display/JENKINS/Jelly+form+controls
+         * Select (drop-down menu), Validation Button
+         */
+        public FormValidation doTestConnection(@QueryParameter("testName") final String testName, 
+                                               @QueryParameter("testType") final String testType,
+                                               @QueryParameter("time") final String time,
+                                               @AncestorInPath AbstractProject project)
+                throws IOException, ServletException {
+            try {
+                if (testType.equals("unit") || testType.equals("regression") || testType.equals("functional")) {
+                    return FormValidation.ok("Success");
+                } else {
+                    return FormValidation.error("Currently test type '" + testType + "' is not supported, pick another one");
+                }
+            } catch (NumberFormatException e) {
+            }
+            return FormValidation.error("Unknown error occurred");
+        }
+
+        /**
+         * https://wiki.jenkins.io/display/JENKINS/Jelly+form+controls
+         * Select (drop-down menu) with model filled values
+         */
+        public ListBoxModel doFillGoalTypeItems() {
+            ListBoxModel items = new ListBoxModel();
+            for (BuildGoal goal : getBuildGoals()) {
+                items.add(goal.getDisplayName(), goal.getId());
+            }
+            return items;
+        }
+
+        public List<BuildGoal> getBuildGoals() {
+            List<BuildGoal> list = new ArrayList<BuildGoal>();
+            list.add(new BuildGoal("", "Select a build goal"));
+            list.add(new BuildGoal("101", "Build"));
+            list.add(new BuildGoal("201", "Package"));
+            list.add(new BuildGoal("301", "Deploy"));
+            list.add(new BuildGoal("401", "Install"));
+            return list;
+        }
+
+        public FormValidation doCheckGoalType(@QueryParameter("goalType") final String goalType, @AncestorInPath AbstractProject project)
+                throws IOException, ServletException {
+            try {
+                if (goalType.equals("101") || goalType.equals("201")) {
+                    return FormValidation.ok();
+                }
+                return FormValidation.error("Currently goal type '" + goalType + "' is not supported, pick another one");
+            } catch (NumberFormatException e) {
+            }
+            return FormValidation.error("Unknown error occurred");
         }
     }
 }
