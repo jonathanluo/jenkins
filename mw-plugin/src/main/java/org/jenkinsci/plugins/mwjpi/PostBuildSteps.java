fd59d61b3a2ade4bf447d680ef942c24ef8e5d11
moonwave <moonwave.systems@gmail.com>
2018-02-04 21:57:23 -0800

    post build steps

--- mw-plugin/src/main/java/org/jenkinsci/plugins/mwjpi/PostBuildSteps.java
+++ mw-plugin/src/main/java/org/jenkinsci/plugins/mwjpi/PostBuildSteps.java
@@ -2,29 +2,30 @@ package org.jenkinsci.plugins.mwjpi;
 
 import hudson.Launcher;
 import hudson.Extension;
-import hudson.FilePath;
 import hudson.util.FormValidation;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Run;
-import hudson.model.TaskListener;
-import hudson.tasks.Builder;
+import hudson.model.BuildListener;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
 import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
-import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundSetter;
 
-public class MwJpiBuilder extends Builder implements SimpleBuildStep {
+public class PostBuildSteps extends Recorder {
 
     private final String name;
     private boolean useFrench;
 
     @DataBoundConstructor
-    public MwJpiBuilder(String name) {
+    public PostBuildSteps(String name) {
         this.name = name;
     }
 
@@ -42,26 +43,32 @@ public class MwJpiBuilder extends Builder implements SimpleBuildStep {
     }
 
     @Override
-    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
         if (useFrench) {
             listener.getLogger().println("Bonjour, " + name + "!");
         } else {
             listener.getLogger().println("Hello, " + name + "!");
         }
+        return true;
     }
 
     @Symbol("greet")
     @Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
         public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
                 throws IOException, ServletException {
             if (value.length() == 0)
-                return FormValidation.error(Messages.MwJpiBuilder_DescriptorImpl_errors_missingName());
+                return FormValidation.error(Messages.PostBuildSteps_DescriptorImpl_errors_missingName());
             if (value.length() < 4)
-                return FormValidation.warning(Messages.MwJpiBuilder_DescriptorImpl_warnings_tooShort());
+                return FormValidation.warning(Messages.PostBuildSteps_DescriptorImpl_warnings_tooShort());
             if (!useFrench && value.matches(".*[éáàç].*")) {
-                return FormValidation.warning(Messages.MwJpiBuilder_DescriptorImpl_warnings_reallyFrench());
+                return FormValidation.warning(Messages.PostBuildSteps_DescriptorImpl_warnings_reallyFrench());
             }
             return FormValidation.ok();
         }
@@ -73,7 +80,7 @@ public class MwJpiBuilder extends Builder implements SimpleBuildStep {
 
         @Override
         public String getDisplayName() {
-            return Messages.MwJpiBuilder_DescriptorImpl_DisplayName();
+            return Messages.PostBuildSteps_DescriptorImpl_DisplayName();
         }
 
     }
